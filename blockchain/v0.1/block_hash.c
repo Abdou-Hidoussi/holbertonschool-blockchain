bef485d9aa4d88c43848907d1c13d4a0f9768832
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2022-01-30 16:26:35 +0100

    initial

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -2,6 +2,6 @@
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
 uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-    sha256((int8_t const *)block, block->data.len, &hash_buf[SHA256_DIGEST_LENGTH]);
+    sha256((int8_t const *)block, block->data.len +  sizeof(block->info), hash_buf);
     return(hash_buf);
 }
