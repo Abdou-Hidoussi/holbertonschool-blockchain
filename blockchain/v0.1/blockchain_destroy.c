68d7634d7d313b605d1c9ecf09230c5503ad3d94
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2022-01-30 16:01:27 +0100

    betty

--- blockchain/v0.1/blockchain_destroy.c
+++ blockchain/v0.1/blockchain_destroy.c
@@ -1,5 +1,10 @@
 #include "blockchain.h"
 
+/**
+ * blockchain_destroy - destroys a blockchain
+ * @blockchain: pointer to the blockchain to be destroyed
+ * Return: void
+ **/
 void blockchain_destroy(blockchain_t *blockchain)
 {
 	llist_destroy(blockchain->chain, 1, NULL);
