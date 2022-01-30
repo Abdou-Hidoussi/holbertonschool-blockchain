b94f9b517efec53eb11dc9eaf9d5b170e062669d
Abdou-Hidoussi <hidoussiabdou5@gmail.com>
2022-01-30 15:49:51 +0100

    initial

---
+++ blockchain/v0.1/block_destroy.c
@@ -0,0 +1,11 @@
+#include "blockchain.h"
+/**
+ * block_destroy - destroys a block
+ * @block: pointer to the block to be destroyed
+ * Return: void
+ * On error, return NULL
+ **/
+void block_destroy(block_t *block)
+{
+	free(block);
+}
